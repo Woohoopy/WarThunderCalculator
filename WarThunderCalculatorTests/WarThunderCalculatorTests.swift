a37d79609fb08d8824efc912e050bd7cf865e381
Woohoopy <woohoopy@aim.com>
2015-07-09 23:49:41 -0500

    Initial Push including a tab view application.

---
+++ WarThunderCalculatorTests/WarThunderCalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  WarThunderCalculatorTests.swift
+//  WarThunderCalculatorTests
+//
+//  Created by John  De Angulo on 7/9/15.
+//  Copyright (c) 2015 WooHops. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WarThunderCalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
