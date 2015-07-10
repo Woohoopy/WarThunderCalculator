a5ee9a41ce88991b32396ff5892eb9a58caebf87
Woohoopy <woohoopy@aim.com>
2015-07-10 12:32:32 -0500

    Commented out code for final calculations.

--- WarThunderCalculator/FirstViewController.swift
+++ WarThunderCalculator/FirstViewController.swift
@@ -36,17 +36,21 @@ class FirstViewController: UIViewController {
     }
 
     @IBAction func goButtonDidClick(sender: UIButton) {
+        // Read values last inputed by user
         var uno = planeOneBR.text.toDouble
         var dos = planeTwoBR.text.toDouble
         var tres = planeThreeBR.text.toDouble
+        // Sort values in array of ascending order
         var brs:[Double] = [uno,dos,tres]
         brs.sort(<)
+        // Any planes with BR difference greater than 0.6 should be changed to 0.6 less than the best plane
         if(brs[2]-brs[1]>0.6){
         brs[1]=brs[2]-0.6
         }
         if(brs[2]-brs[0]>0.6){
         brs[0]=brs[2]-0.6
         }
+        
         var total = (brs[2]/2)+(brs[0]+brs[1])/4
         totalRankLabel.text = "Total Rank"
         totalRankNumberLabel.text=("\(total)")
