12a122b0ddd10f72791381d7dba55bd6a09c28e1
Woohoopy <woohoopy@aim.com>
2015-07-10 15:37:47 -0500

    Fixed #2 and #5 by adding keyboard controls, font color alphas, and numpad with decimal.

--- WarThunderCalculator/FirstViewController.swift
+++ WarThunderCalculator/FirstViewController.swift
@@ -27,21 +27,20 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        totalRankLabel.text = ""
+        totalRankNumberLabel.text = ""
+        
         self.planeOneBR.delegate = self
         self.planeTwoBR.delegate = self
         self.planeThreeBR.delegate = self
     }
     
-    func textField(textField: UITextField,
-        shouldChangeCharactersInRange range: NSRange,
-        replacementString string: String) -> Bool {
-            
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
             // Get the attempted new string by replacing the new characters in the
             // appropriate range
             let newString = (textField.text as NSString).stringByReplacingCharactersInRange(range, withString: string)
             
             if count(newString) > 0 {
-                
                 // Find out whether the new string is numeric by using an NSScanner.
                 // The scanDecimal method is invoked with NULL as value to simply scan
                 // past a decimal integer representation.
@@ -51,11 +50,9 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
                 return isNumeric
                 
             } else {
-                
                 // To allow for an empty text field
                 return true
             }
-            
     }
 
     func textFieldShouldReturn(textField: UITextField) -> Bool {
