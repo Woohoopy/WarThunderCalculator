eb90bb2a5172541601a3fa149d15698c992f06ae
Woohoopy <woohoopy@aim.com>
2015-07-10 02:31:21 -0500

    Works with variables along with a functioning button and output message.

--- WarThunderCalculator/FirstViewController.swift
+++ WarThunderCalculator/FirstViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    @IBOutlet weak var goButton: UIButton!
+    @IBOutlet weak var planeOneBR: UITextField!
+    @IBOutlet weak var planeTwoBR: UITextField!
+    @IBOutlet weak var planeThreeBR: UITextField!
+    @IBOutlet weak var totalRankLabel: UILabel!
+    @IBOutlet weak var totalRankNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        totalRankLabel.text = "";
+        totalRankNumberLabel.text = "";
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,17 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func goButtonDidClick(sender: UIButton) {
+        var uno = (planeOneBR.text as NSString).doubleValue
+        var dos = (planeTwoBR.text as NSString).doubleValue
+        var tres = (planeThreeBR.text as NSString).doubleValue
+        
+        var total = (uno/2)+(dos+tres)/4
+        totalRankLabel.text = "Total Rank"
+        totalRankNumberLabel.text=("\(total)")
+        
+        
+    }
 
 }
 
