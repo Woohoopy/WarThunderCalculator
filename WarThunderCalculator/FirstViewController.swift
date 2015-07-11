e009fae8641ba959f5cd06434274e3507fa45318
Woohoopy <woohoopy@aim.com>
2015-07-11 17:03:31 -0500

    Rudimentary scroll animation.

--- WarThunderCalculator/FirstViewController.swift
+++ WarThunderCalculator/FirstViewController.swift
@@ -57,6 +57,8 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var planeThreeBR: UITextField!
     @IBOutlet weak var totalRankLabel: UILabel!
     @IBOutlet weak var totalRankNumberLabel: UILabel!
+    
+    var kbHeight: CGFloat!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -87,6 +89,40 @@ class FirstViewController: UIViewController, UITextFieldDelegate {
     
     override func viewWillLayoutSubviews() {
         nextButtonInputAccessoryView.frame = CGRect(x: 0, y: 0, width: CGRectGetWidth(view.bounds), height: Constants.kNextButtonAcessoryInputAccessoryViewHeight)
+    }
+    
+    override func viewWillAppear(animated:Bool) {
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize =  (userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+                kbHeight = keyboardSize.height
+                self.animateTextField(true)
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        self.animateTextField(false)
+    }
+    
+    func animateTextField(up: Bool) {
+        var movement = (up ? -kbHeight : kbHeight)
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.view.frame = CGRectOffset(self.view.frame, 0, movement)
+        })
     }
     
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
